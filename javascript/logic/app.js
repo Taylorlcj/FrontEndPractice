a3e866875c17b570c87044487621ded2ec272f78
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-20 23:59:09 -0500

    logic

--- javascript/logic/app.js
+++ javascript/logic/app.js
@@ -1,6 +1,22 @@
+//AND
 const password = prompt("Enter your password");
 if (password.length >= 6 && password.indexOf(" ") === -1) {
 	console.log("VALID RESPONSE");
 } else {
 	console.log("Incorrect format for response");
 }
+
+//OR
+const age = 99;
+if ((age >= 0 && age < 5) || age >= 65) {
+	console.log("free");
+} else if (age >= 5 && age < 10) {
+	console.log("$10");
+} else if (age >= 10 && age < 65) {
+	console.log("$20");
+} else {
+	console.log("Invalid age");
+}
+
+//NOT
+
