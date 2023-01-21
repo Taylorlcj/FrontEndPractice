81eb558b0438d8e7c6316dcff1cc1563e75ef1b3
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-20 23:35:37 -0500

    adds semi colons

--- javascript/conditionals/app.js
+++ javascript/conditionals/app.js
@@ -1,51 +1,51 @@
 let rand = Math.random();
 if (rand < 0.5) {
-    console.log("Your number is less than 0.5!")
-    console.log(rand)
+	console.log("Your number is less than 0.5!");
+	console.log(rand);
 }
 
 if (rand >= 0.5) {
-    console.log("Your number is greater or equal to than 0.5!")
-    console.log(rand)
+	console.log("Your number is greater or equal to than 0.5!");
+	console.log(rand);
 }
 // ELSE IF
 const age = 8;
 
 if (age < 5) {
-    console.log("You are a baby, you get in for free")
+	console.log("You are a baby, you get in for free");
 } else if (age < 10) {
-    console.log("You are a child, you pay 10 dollars")
+	console.log("You are a child, you pay 10 dollars");
 } else if (age < 65) {
-    console.log("You are an adult, you pay 8000 dollars")
+	console.log("You are an adult, you pay 8000 dollars");
 }
 
 //ELSE(If nothing else is true, do this)
 const dayOfWeek = prompt("Enter a day:").toLowerCase();
 
 if (dayOfWeek === "monday") {
-    console.log("UGH I HATE MONDAYS")
+	console.log("UGH I HATE MONDAYS");
 } else if (dayOfWeek === "saturday") {
-    console.log()
+	console.log();
 } else if (dayOfWeek === "friday") {
-    console.log("Fridays are decent")
+	console.log("Fridays are decent");
 } else {
-    console.log("Meh")
+	console.log("Meh");
 }
 
 //NESTING CONDITIONALS
-const password = prompt("please enter a new password")
+const password = prompt("please enter a new password");
 
-// Password must be 6+ characters 
+// Password must be 6+ characters
 if (password.length >= 6) {
-    console.log("long enough password")
+	console.log("long enough password");
 } else {
-    console.log("Password too short! Must be 6+ characters")
+	console.log("Password too short! Must be 6+ characters");
 }
 
 //Password cannot include space
 
-if(password.indexOf(" ") === -1) {
-    console.log("You have no spaces, good job")
-}else {
-    console.log("Passwords cannot have spaces")
+if (password.indexOf(" ") === -1) {
+	console.log("You have no spaces, good job");
+} else {
+	console.log("Passwords cannot have spaces");
 }
