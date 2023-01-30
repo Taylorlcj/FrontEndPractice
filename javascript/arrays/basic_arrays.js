a5cca20c13c5db5a86368f9d16aae643da49b1cd
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-30 00:02:51 -0500

    note to self, run js with node in vscode terminal

---
+++ javascript/arrays/basic_arrays.js
@@ -0,0 +1,16 @@
+//empty array
+let students = [];
+
+//array of strings
+let colors = ["red", "orange", "yellow"];
+
+//array of ints
+let lottoNums = [19, 22, 56, 12, 51];
+
+//array of mix
+let stuff = [true, 68, "cat", null];
+
+console.log(lottoNums[0]) //prints 19
+
+
+
---
+++ javascript/arrays/change_arrays.js
@@ -0,0 +1,15 @@
+let colors = ["rad", "orange", "green", "yellow"];
+
+console.log(colors);
+
+colors[0] = "red";
+
+colors[2] = "yellow";
+
+colors[3] = "green’";
+
+colors[4];
+
+colors[4] = "blue";
+
+console.log(colors);
