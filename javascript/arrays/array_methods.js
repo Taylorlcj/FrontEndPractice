8b34d9134a1aa5c28feb00c8ebcf9f6e0eb0256c
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-30 00:20:34 -0500

    array methods

--- javascript/arrays/array_methods.js
+++ javascript/arrays/array_methods.js
@@ -2,3 +2,29 @@
 // Pop - remove from end
 // Shift - remove from start
 // Unshift - add to start
+
+let movieLine = ["tom", "nancy", "pablo"];
+console.log(movieLine);
+
+movieLine.push("Oliver");
+console.log(movieLine);
+
+movieLine.pop();
+console.log(movieLine);
+
+let barbell = [];
+
+barbell.push(45);
+barbell.push(45);
+barbell.push(85);
+barbell.push(5);
+barbell.push(65);
+console.log(barbell);
+
+let cinemaQueue = [];
+cinemaQueue.push("Jenna", "Mary", "Jackson", "Holden");
+console.log(cinemaQueue);
+
+cinemaQueue.shift();
+cinemaQueue.unshift("Dustin");
+console.log(cinemaQueue);
