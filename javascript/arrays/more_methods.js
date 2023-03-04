2ceb01a9ae478e5b30946bd878762dacb8076b11
Taylor Jensen <Taylorlcj@gmail.com>
2023-03-04 13:47:33 -0500

    js array methods

--- javascript/arrays/more_methods.js
+++ javascript/arrays/more_methods.js
@@ -22,6 +22,7 @@ console.log(combo.indexOf("Blue")); //There ya go
 combo.reverse(); //reorganizes the array permanently
 console.log(combo);
 
+//SLICE
 let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 let coolColors = colors.slice(3); //from index 3 onwards
 console.log(coolColors);
@@ -31,5 +32,13 @@ console.log(warmColors);
 
 let lastThree = colors.slice(-3); //stores the last 3
 console.log(lastThree);
+
+//SPlICE
 colors.splice(5, 1); //deletes indigo(starts at index 5 and deletes 1 thing)
+console.log(colors);
+
+colors.splice(1, 0, "red-orange"); //starts at index 1, deletes 0 items, adds "red-orange"
+console.log(colors);
+
+colors.splice(4, 0, "yellow-green", "forest-green"); //starts at index 4, deletes 0 items, adds two items
 console.log(colors);
