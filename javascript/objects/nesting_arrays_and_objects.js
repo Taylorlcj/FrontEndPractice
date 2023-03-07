ff35e3d450bfd71df63fc68739b39b5d76ca6a45
Taylor Jensen <Taylorlcj@gmail.com>
2023-03-06 19:58:03 -0500

    done with objects

---
+++ javascript/objects/nesting_arrays_and_objects.js
@@ -0,0 +1,10 @@
+const comments = [
+	{ username: "Tammy", text: "lolololol", votes: 9 },
+	{ username: "Fishboi", text: "glubglub", votes: 11 },
+];
+
+//how to access glubglub
+console.log(comments[1].text);
+
+//how to access Tammy
+console.log(comments[0].username);
